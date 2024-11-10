b8573cce8cd74267b5debfdc2f7beac6b7f22cd3
esoloveva <elvira.solov@inbox.ru>
2024-11-10 21:30:59 +0300

    fix: заглушка вынесена в отдельный компонент

--- src/components/offers-list/OffersList.tsx
+++ src/components/offers-list/OffersList.tsx
@@ -9,6 +9,7 @@ import { SortName } from 'types/types';
 import Spinner from '@components/spinner/Spinner';
 import { getCity, getSorting } from '@store/site-process/selectors';
 import { getIsOffersLoading, selectOffers } from '@store/site-data/selectors';
+import OffersListEmpty from './OffersListEmpty';
 
 const OffersList = (): JSX.Element => {
   const activeCity = useAppSelector(getCity);
@@ -38,20 +39,9 @@ const OffersList = (): JSX.Element => {
   }
 
   if (offers.length === 0) {
-    return (
-      <div className="cities__places-container cities__places-container--empty container">
-        <section className="cities__no-places">
-          <div className="cities__status-wrapper tabs__content">
-            <b className="cities__status">No places to stay available</b>
-            <p className="cities__status-description">
-              We could not find any property available at the moment in {activeCity.name}
-            </p>
-          </div>
-        </section>
-        <div className="cities__right-section"></div>
-      </div>
-    );
+    return <OffersListEmpty city={activeCity.name} />;
   }
+
   return (
     <div className="cities__places-container container">
       <section className="cities__places places">
